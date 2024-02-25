201474b3e969f6cb71ad20429f16843350e236b6
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2024-02-25 02:43:53 -0300

    Update cpulimiter.sh

--- cpulimiter.sh
+++ cpulimiter.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Host do servidor remoto
+HOST=$(hostname -I | awk '{print $1}')
+
 # Função para ativar cpulimit em um processo
 ativar_cpulimit() {
   echo "Digite o PID do processo que deseja limitar:"
@@ -16,7 +19,7 @@ ativar_cpulimit() {
     return
   fi
 
-  sshplus "cpulimit --pid $pid --limit $limite_cpu" >/dev/null 2>&1 &
+  ssh -t $HOST "sudo cpulimit --pid $pid --limit $limite_cpu" >/dev/null 2>&1 &
   echo "cpulimit ativado para o processo com PID $pid com limite de $limite_cpu%."
 }
 
@@ -29,34 +32,28 @@ desativar_cpulimit() {
     return
   fi
 
-  sshplus "kill -15 $pid" >/dev/null 2>&1
+  ssh -t $HOST "sudo kill -15 $pid" >/dev/null 2>&1
   echo "cpulimit desativado para o processo com PID $pid."
 }
 
 # Função para identificar e parar processos que estão consumindo muita CPU
 parar_processos_cpu_alta() {
-  echo "Digite o limite percentual de uso da CPU para identificar os processos (0-100):"
-  read limite_cpu
-  if [ $limite_cpu -lt 0 ] || [ $limite_cpu -gt 100 ]; then
-    echo "O limite percentual deve estar entre 0 e 100."
+  echo "Identificando processos com uso de CPU em 100%..."
+  processos_100_cpu=$(ssh -t $HOST "ps aux --sort=-%cpu" | awk '$3 == 100 {print $2}')
+  if [ -z "$processos_100_cpu" ]; then
+    echo "Não foram encontrados processos com uso de CPU em 100%."
     return
   fi
 
-  processos_alta_cpu=$(sshplus "ps aux --sort=-%cpu" | awk -v lim=$limite_cpu '$3 >= lim {print $2}')
-  if [ -z "$processos_alta_cpu" ]; then
-    echo "Não foram encontrados processos com uso de CPU acima de $limite_cpu%."
-    return
-  fi
+  echo "Processos com uso de CPU em 100%:"
+  echo "$processos_100_cpu"
 
-  echo "Processos com uso de CPU acima de $limite_cpu%:"
-  echo "$processos_alta_cpu"
-
-  echo "Deseja encerrar esses processos? (s/n)"
+  echo "Deseja encerrar esses processos automaticamente? (s/n)"
   read resposta
   if [ "$resposta" = "s" ]; then
     echo "Encerrando processos..."
-    for pid in $processos_alta_cpu; do
-      sshplus "kill -15 $pid" >/dev/null 2>&1
+    for pid in $processos_100_cpu; do
+      ssh -t $HOST "sudo kill -9 $pid" >/dev/null 2>&1
     done
     echo "Processos encerrados."
   else
@@ -70,13 +67,15 @@ while true; do
   echo "1. Ativar cpulimit"
   echo "2. Desativar cpulimit"
   echo "3. Identificar e parar processos com alto uso de CPU"
-  echo "4. Sair"
+  echo "4. Matar processos com uso de CPU em 100%"
+  echo "5. Sair"
   read -p "Escolha uma opção: " opcao
   case $opcao in
     1) ativar_cpulimit ;;
     2) desativar_cpulimit ;;
     3) parar_processos_cpu_alta ;;
-    4) exit ;;
+    4) matar_processos_100_cpu ;;
+    5) exit ;;
     *) echo "Opção inválida. Tente novamente." ;;
   esac
 done
